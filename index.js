179c1b4180e32d533610a9369c47a8610c118a01
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-14 05:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ console.log(unleashDog);
 function exerciseDog(dogName, dogBreed){
   for(let i=0; i<routine.length; i++){
    // routine[i](dogName, dogBreed);
-  routine[i]().call(dogName, dogBreed)
-   let z = console.log(routine[i](dogName, dogBreed));
+  routine[i]().call(dogName, dogBreed);
+   let z = 
    let res=[];
    res.push(z);
   }
