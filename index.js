a09d6e40b46da5b9dfd10647a8dcc4fd3bf4dd38
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-14 04:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ function wakeDog(dogName, dogBreed){
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 function leashDog(){
-  console.log(`Leash Byron the poodle`);
+  console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 function walkDog(){
-  console.log("Walk to the park with Byron the poodle");
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 function throwDog(){
-  console.log("Throw the frisbee for Byron the poodle");
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 function walkHomeDog(){
-  console.log("Walk home with Byron the poodle");
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
 function unleashDog(){
-  console.log("Unleash Byron the poodle");
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
 }
