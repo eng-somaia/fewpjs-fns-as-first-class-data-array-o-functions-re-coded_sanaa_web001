35a43fae1a4a8a28507584afb90694a26533d88a
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-14 04:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function leashDog(){
 function walkDog(){
   console.log("Walk to the park with Byron the poodle");
 }
-function wakeDog(){
-  console.log("Wake Byron the poodle");
+function throwDog(){
+  console.log("Throw the frisbee for Byron the poodle");
 }
 function wakeDog(){
   console.log("Wake Byron the poodle");
